900609e907f9f988e49d707eb450e0e1b3e12c1d
Hakan <dgnhakan1@gmail.com>
2023-08-02 13:25:31 +0300

    Add project files.

---
+++ AjaxProject/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using AjaxProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AjaxProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CustomerList()
+        {
+            var jsonCustomer = JsonConvert.SerializeObject(context.Customers.ToList());
+            return Json(jsonCustomer);
+        }
+        public IActionResult AddCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+            var values = JsonConvert.SerializeObject(customer);
+            context.SaveChanges();
+            return Json(values);
+        }
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = context.Customers.Where(x=>x.CustomerID==id).FirstOrDefault();
+            context.Customers.Remove(value);
+            context.SaveChanges();
+            return NoContent();
+
+        }
+        public IActionResult GetCustomer(int id)
+        {
+            var value = context.Customers.FirstOrDefault(x=>x.CustomerID==id);
+            var jsonCustomer=JsonConvert.SerializeObject(value);
+            return Json(jsonCustomer);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            context.Customers.Update(customer);
+            var values = JsonConvert.SerializeObject(customer);
+            context.SaveChanges();
+            return Json(values);
+        }
+    }
+}
---
+++ AjaxProject/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-NOMRM5V\\SQLEXPRESS;initial catalog=AjaxDb;integrated security=true;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
+
+
